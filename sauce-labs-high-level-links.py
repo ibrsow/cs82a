e35dd692e278934e711a57b83b9502af6c3f222e
ibrsow <ibsow@yahoo.com>
2015-06-18 23:13:02 -0700

    Update sauce-labs-high-level-links.py

--- sauce-labs-high-level-links.py
+++ sauce-labs-high-level-links.py
@@ -1,48 +1,108 @@
 # -*- coding: utf-8 -*-
 # CS82A - Automation Project - Ibrahima Sow
+# Version 2
 #
 # This module Create a Selenium test to automate the checking of the high-level links behind
 # the three-horizontal-bar glyph in the header of the Sauce Labs home page: RESOURCES, PRICING,
 # FEATURES, DOCS, SIGN UP, COMPANY, ENTERPRISE, LOGIN, COMMUNITY, and SOLUTIONS.
 
-
 from selenium import webdriver
 import unittest
 
-links_landing_titles = {
-    "RESOURCES"  : r'Resources',
-    "PRICING"    : r'Pricing',
-    "FEATURES"   : r'Features',
-    "DOCS"       : r'Docs',
-    "SIGN UP"    : r'SignXXXX',
-    "COMPANY"    : r'Company',
-    "ENTERPRISE" : r'Enterprise',
-    "LOGIN"      : r'Login',
-    "COMMUNITY"  : r'Community',
-    "SOLUTIONS"  : r'Solutions',
+links_landing_pages = {
+    "RESOURCES"  : "//a[contains(text(),'Resources')]",        # <a href="https://saucelabs.com/resources" title="Resources">
+    "PRICING"    : "//a[contains(text(),'Pricing')])[3]",      # <a title="Pricing" href="/pricing">
+    "FEATURES"   : "//a[contains(text(),'Features')])[3]",     # <a href="/features">
+    "DOCS"       : "//a[contains(text(),'Docs')])[3]",         # <a href="https://docs.saucelabs.com/?_ga=1.138110450.398500475.1434607228" title="Docs">
+    "SIGN UP"    : "//a[contains(text(),'Sign up')]",          # <a href="https://saucelabs.com/signup/trial" title="Sign up">
+    "COMPANY"    : "//a[contains(text(),'Company')])[2]",      # <a href="https://saucelabs.com/our-values">
+    "ENTERPRISE" : "//a[contains(text(),'Enterprise')])[3]",   # <a href="https://saucelabs.com/enterprise" title="Enterprise">
+    "LOGIN"      : "//a[contains(text(),'Login')]",            # <a href="https://saucelabs.com/login" title="Log in">
+    "COMMUNITY"  : "//a[contains(text(),'Community')]",        # <a href="https://saucelabs.com/opensauce" title="Community">
+    "SOLUTIONS"  : "//a[contains(text(),'Solutions')]",        # <a href="https://saucelabs.com/selenium" title="Solutions">
     }
-
 
 class SauceLabsHeaders(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "https://saucelabs.com/"
-        self.verificationErrors = []
-    
+
     def test_sauce_labs_headers(self):
         driver = self.driver
+        glyph  = "//a[contains(@href, '#')]"
 
-        for link in links_landing_titles:
+        for link in links_landing_pages:
             driver.get(self.base_url)
+            driver.find_element_by_xpath(glyph).click()
             driver.find_element_by_link_text(link).click()
-            expected_title = links_landing_titles[link]
-            try: self.assertRegexpMatches(driver.title, expected_title)
-            except AssertionError as e: self.verificationErrors.append(str(e))
 
     def tearDown(self):
+        self.driver.implicitly_wait(30)
         self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
 
+'''
+# -*- coding: utf-8 -*-
+# CS82A - Automation Project - Ibrahima Sow
+# Version 1
+
+class SauceLabsHeaders(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://saucelabs.com/"
+        glyph         = "//a[contains(@href, '#')]"
+
+    def test_sauce_labs_headers(self):
+        driver = self.driver
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("//a[contains(text(),'Resources')]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Pricing')])[3]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Features')])[3]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Docs')])[3]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("//a[contains(text(),'Sign up')]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Company')])[2]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("(//a[contains(text(),'Enterprise')])[3]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("//a[contains(text(),'Login')]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("//a[contains(text(),'Community')]").click()
+
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(glyph).click()
+        driver.find_element_by_xpath("//a[contains(text(),'Solutions')]").click()
+
+    def tearDown(self):
+        self.driver.implicitly_wait(30)
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+'''
